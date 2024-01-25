afd155dec6fdb2f0f0046e5c307ab0bac6af4c2d
emilian <buduganemil@yahoo.com>
2024-01-25 21:29:07 +0200

    2nd commit - com.demoqa - BasePageTest

--- src/test/java/com/demoqa/BasePageTest.java
+++ src/test/java/com/demoqa/BasePageTest.java
@@ -2,6 +2,8 @@ package com.demoqa;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -9,15 +11,25 @@ import org.testng.annotations.Parameters;
 public class BasePageTest {
     public WebDriver driver;
 
-    @Parameters({"testPageUrl"})
+    @Parameters({"browser", "testPageUrl"})
     @BeforeTest
-    public void driverSetup(String testPageUrl) {
-        driver = new ChromeDriver();
+    public void driverSetup(String browser, String testPageUrl) {
+
+        switch (browser) {
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            default:
+                driver = new ChromeDriver();
+        }
         driver.get(testPageUrl);
         driver.manage().window().maximize();
     }
 
-    @AfterTest
+    @AfterTest(alwaysRun = true)
     public void tearDown() {
         driver.quit();
     }
