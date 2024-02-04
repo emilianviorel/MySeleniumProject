08215bbc90324d1890aa6cc11cd32d24c6978c6b
emilian <buduganemil@yahoo.com>
2024-02-04 14:51:31 +0200

    9th commit - com.demoqa - UploadDownloadPageTest

---
+++ src/test/java/com/demoqa/UploadDownloadPageTest.java
@@ -0,0 +1,41 @@
+package com.demoqa;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class UploadDownloadPageTest extends BasePageTest {
+
+    public final By UPLOAD_FILE_BUTTON = By.id("uploadFile");
+    public final By UPLOADED_FILE_PATH = By.id("uploadedFilePath");
+    public final By DOWNLOAD_FILE_BUTTON = By.id("downloadButton");
+    public final String TXT_FILE_PATH = System.getProperty("user.dir")
+            + "/src/test/resources/TestUpload.txt";
+
+    @Test(priority = 1)
+    public void testUploadFileButtonFunctionality() {
+
+        driver.findElement(UPLOAD_FILE_BUTTON).sendKeys(TXT_FILE_PATH);
+        Assert.assertTrue(driver.findElement(UPLOADED_FILE_PATH).isDisplayed());
+    }
+
+    @Test(priority = 2)
+    public void testDownloadFileButtonFunctionality() {
+
+        driver.findElement(DOWNLOAD_FILE_BUTTON).click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String downloadFilePath = System.getProperty("user.home") + "/Downloads/sampleFile.jpeg";
+        System.out.println(downloadFilePath);
+        File downloadedFile = new File(downloadFilePath);
+        Assert.assertTrue(downloadedFile.exists(), "Downloaded file does not exist");
+
+    }
+}
